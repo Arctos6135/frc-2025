8b40f5488124f90947e8ff2c6778d710c706bf0f
Ben M <benjt.mcmillan@icloud.com>
2025-01-23 13:05:25 -0500

    should drive now?

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,9 +1,14 @@
 package frc.robot;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.commands.TeleopDrive;
 import frc.robot.constants.ControllerConstants;
+import frc.robot.subsystems.drivetrain.Drivetrain;
+import frc.robot.subsystems.drivetrain.DrivetrainIO;
+
 import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 
 public class RobotContainer {
@@ -15,7 +20,16 @@ public class RobotContainer {
   public LoggedDashboardChooser<Command> autoChooser;
   public LoggedDashboardChooser<Pose2d> positionChooser;
 
+  public Drivetrain drivetrain;
+  public TeleopDrive teleopDrive;
+
   public RobotContainer() {
+    if (RobotBase.isReal()){
+      drivetrain = new Drivetrain(new DrivetrainIO());
+    }
+
+    teleopDrive = new TeleopDrive(drivetrain, driverController);
+    drivetrain.setDefaultCommand(teleopDrive);
 
     configureAuto();
     configureBindings();
--- src/main/java/frc/robot/commands/TeleopDrive.java
+++ src/main/java/frc/robot/commands/TeleopDrive.java
@@ -1,21 +1,18 @@
 package frc.robot.commands;
 
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.drivetrain.Drivetrain;
 import java.util.function.DoubleSupplier;
 import swervelib.math.SwerveMath;
 
 public class TeleopDrive extends Command {
-  private DoubleSupplier translationX;
-  private DoubleSupplier translationY;
-  private DoubleSupplier angularRotationX;
+  private XboxController controller;
   private Drivetrain drivetrain;
 
-  public TeleopDrive(Drivetrain drivetrain, DoubleSupplier translationX, DoubleSupplier translationY, DoubleSupplier angularRotationX) {
-    this.translationX = translationX;
-    this.translationY = translationY;
-    this.angularRotationX = angularRotationX;
+  public TeleopDrive(Drivetrain drivetrain, XboxController controller) {
+    this.controller = controller;
     this.drivetrain = drivetrain;
 
     addRequirements(drivetrain);
@@ -26,10 +23,10 @@ public class TeleopDrive extends Command {
     drivetrain.swerveDrive.drive(
         SwerveMath.scaleTranslation(
             new Translation2d(
-                translationX.getAsDouble() * drivetrain.swerveDrive.getMaximumChassisVelocity(),
-                translationY.getAsDouble() * drivetrain.swerveDrive.getMaximumChassisVelocity()),
+                controller.getRawAxis(XboxController.Axis.kLeftX.value) * drivetrain.swerveDrive.getMaximumChassisVelocity(),
+                controller.getRawAxis(XboxController.Axis.kLeftY.value) * drivetrain.swerveDrive.getMaximumChassisVelocity()),
             0.8),
-        Math.pow(angularRotationX.getAsDouble(), 3) * drivetrain.swerveDrive.getMaximumChassisAngularVelocity(),
+        Math.pow(controller.getRawAxis(XboxController.Axis.kRightX.value), 3) * drivetrain.swerveDrive.getMaximumChassisAngularVelocity(),
         true,
         false);
   }
