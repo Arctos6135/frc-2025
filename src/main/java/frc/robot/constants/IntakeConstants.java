fff232b06dc3f02ee3c77da6428404b91af61e81
michewko <micah.hewko@gmail.com>
2025-02-06 15:53:59 -0500

    Implemented intake conversion factors.

--- src/main/java/frc/robot/constants/IntakeConstants.java
+++ src/main/java/frc/robot/constants/IntakeConstants.java
@@ -1,9 +1,15 @@
 package frc.robot.constants;
 
+import edu.wpi.first.math.util.Units;
+
 public class IntakeConstants {
   public static final int CURRENT_LIMIT = 30;
-  public static double POSITION_CONVERSION_FACTOR = 0; // TODO
-  public static double VELOCITY_CONVERSION_FACTOR = 0; // TODO
 
-  public static int MEDIAN_FILTER_SIZE = 12;
+  public static final double GEARBOX_RATIO = 0.0; //TODO
+  // Rotations.
+  public static final double POSITION_CONVERSION_FACTOR = 1 / GEARBOX_RATIO; 
+  // Rotations per second.
+  public static final double VELOCITY_CONVERSION_FACTOR = POSITION_CONVERSION_FACTOR / 60.0; 
+
+  public static final int MEDIAN_FILTER_SIZE = 12;
 }
