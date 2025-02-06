9d96913a43a8d96ac8016c1dd6d1123d65583d90
michewko <micah.hewko@gmail.com>
2025-02-06 16:24:13 -0500

    Implemented the elevator command.

--- src/main/java/frc/robot/constants/ElevatorConstants.java
+++ src/main/java/frc/robot/constants/ElevatorConstants.java
@@ -14,4 +14,10 @@ public class ElevatorConstants {
   public static final double VELOCITY_CONVERSION_FACTOR = POSITION_CONVERSION_FACTOR / 60.0;
   // PID constants for position control.
   public static final double[] PID = {1.0, 0.0, 0.0};
+
+  // The elevator height to score at each level.
+  public static final double L1_HEIGHT = 0.0; //TODO
+  public static final double L2_HEIGHT = 0.0; //TODO
+  public static final double L3_HEIGHT = 0.0; //TODO
+  public static final double L4_HEIGHT = 0.0; //TODO
 }
