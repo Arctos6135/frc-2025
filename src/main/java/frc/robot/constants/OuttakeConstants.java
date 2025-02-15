428845a2842f0a9ed6f19b5cf9abceef230364dc
May <40745122+SomewhatMay@users.noreply.github.com>
2025-02-15 15:33:40 -0500

    outtake::setRPS and Logger in Outtake class #15 #3

--- src/main/java/frc/robot/constants/OuttakeConstants.java
+++ src/main/java/frc/robot/constants/OuttakeConstants.java
@@ -2,7 +2,12 @@ package frc.robot.constants;
 
 public class OuttakeConstants {
   public static final int CURRENT_LIMIT = 10;
+  public static final int MEDIAN_FILTER_SIZE = 12;
+
   public static final int VELOCITY_CONVERSION_FACTOR = -1;
   public static final int POSITION_CONVERSION_FACTOR = -1;
+
   public static final int INTAKE_RPS = 20;
+
+  public static final double[] PID_CONSTANTS = {1.0, 0.0, 0.0};
 }
