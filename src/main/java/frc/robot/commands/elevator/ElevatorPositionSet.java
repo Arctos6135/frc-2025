5f49a1c7cf8dec8d382dda460e1ebeb278bcb056
Kushi Prashanth Sreepuram <sreepuram.kushi@gmail.com>
2025-02-18 00:55:02 -0500

    fixed elevator controls

--- src/main/java/frc/robot/commands/elevator/ElevatorPositionSet.java
+++ src/main/java/frc/robot/commands/elevator/ElevatorPositionSet.java
@@ -4,16 +4,17 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.constants.ElevatorConstants;
 import frc.robot.subsystems.elevator.Elevator;
 import edu.wpi.first.math.controller.PIDController;
-import frc.robot.utils.MathUtils;
 
 public class ElevatorPositionSet extends Command {
   public double setpoint;
+  public double timeAtSetpoint;
   public Elevator elevator;
   public PIDController pidController = new PIDController(ElevatorConstants.PID[0], ElevatorConstants.PID[1], ElevatorConstants.PID[2]);
   
-  public ElevatorPositionSet(Elevator elevator, Double setpoint) {
+  public ElevatorPositionSet(Elevator elevator, Double setpoint, Double timeAtSetpoint) {
     this.elevator = elevator;
     this.setpoint = setpoint;
+    this.timeAtSetpoint = timeAtSetpoint;
 
     addRequirements(elevator);
   }
@@ -22,21 +23,21 @@ public class ElevatorPositionSet extends Command {
   
   public void initialise(){
     elevator.setPosition(setpoint);
-    double targetAtSetpoint = 0;
+    timeAtSetpoint = 0;
   }
 
   @Override
   public void execute(){
       elevator.setVoltage(elevator.calculatePID());
-      if (pidController.atTarget())
-        timeAtTarget += 1;
+      if (pidController.atSetpoint())
+        timeAtSetpoint += 1;
       else 
-        timeAtTarget = 0;
+        timeAtSetpoint = 0;
       
   }
 
   @Override
   public boolean isFinished() {
-    return timeAtTarget >= 25;
+    return timeAtSetpoint >= 25;
   }
 }
