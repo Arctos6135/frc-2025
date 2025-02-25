5ee4e241ef68982c64a3dfc6f68f4b08c8205883
Ben is dying right now <benjt.mcmillan@icloud.com>
2025-02-25 16:59:08 -0500

    Elevator and drivetrain working

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "frc-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 94;
-  public static final String GIT_SHA = "8c60ab8190d3ef0e27f7293eb523512bb81a31c0";
-  public static final String GIT_DATE = "2025-02-25 15:28:13 EST";
+  public static final int GIT_REVISION = 95;
+  public static final String GIT_SHA = "00449da3790ae4bef4cc1312daed5de4b40956a7";
+  public static final String GIT_DATE = "2025-02-25 16:16:56 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-02-25 16:05:32 EST";
-  public static final long BUILD_UNIX_TIME = 1740517532527L;
+  public static final String BUILD_DATE = "2025-02-25 16:57:39 EST";
+  public static final long BUILD_UNIX_TIME = 1740520659418L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
