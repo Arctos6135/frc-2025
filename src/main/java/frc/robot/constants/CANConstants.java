50c364becd426131ef347dc6c2abd33e4e00a6f8
Ben is dying right now <benjt.mcmillan@icloud.com>
2025-02-25 18:00:33 -0500

    can ids

--- src/main/java/frc/robot/constants/CANConstants.java
+++ src/main/java/frc/robot/constants/CANConstants.java
@@ -1,7 +1,7 @@
 package frc.robot.constants;
 
 public class CANConstants {
-  public static final int INTAKE_MOTOR = 9;
+  public static final int INTAKE_MOTOR = 14;
   public static final int ELEVATOR_LEFT = 10;
   public static final int ELEVATOR_RIGHT = 11;
   public static final int OUTTAKE_LEFT = 12;
