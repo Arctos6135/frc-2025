e6e034849a6ddc732005262ab449eda149f02dad
michewko <micah.hewko@gmail.com>
2025-02-28 14:52:11 -0500

    Added a ramp voltage command for characterization.

---
+++ src/main/java/frc/robot/commands/outtake/OuttakeRamp.java
@@ -0,0 +1,40 @@
+package frc.robot.commands.outtake;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.subsystems.intake.Intake;
+import frc.robot.subsystems.outtake.Outtake;
+
+public class OuttakeRamp extends Command {
+    private final Outtake outtake;
+    private double voltage;
+    private double step = 0.1;
+
+    public OuttakeRamp(Outtake outtake) {
+        this.outtake = outtake;
+
+        addRequirements(outtake);
+    }
+
+    @Override
+    public void initialize() {
+        voltage = 0.0;
+        voltage = voltage + step;
+    }
+
+    @Override
+    public void execute() {
+        voltage += step;
+        outtake.setVoltage(voltage);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return voltage >= 12.0;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        outtake.setVoltage(0);
+    }
+}
