78c5cda81b6f0c1c01da19b1ffad76eb9ef8ef6e
Ben M <benjt.mcmillan@icloud.com>
2025-03-02 12:16:30 -0500

    Added some autos

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "frc-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 123;
-  public static final String GIT_SHA = "9a121af8d96de65d9098c1d78067daf3838d71e7";
-  public static final String GIT_DATE = "2025-02-28 16:17:01 EST";
+  public static final int GIT_REVISION = 125;
+  public static final String GIT_SHA = "eb7fa9b5798fd2b5e6372c979266e14a35906ce1";
+  public static final String GIT_DATE = "2025-03-01 16:53:30 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-02-28 16:38:43 EST";
-  public static final long BUILD_UNIX_TIME = 1740778723938L;
+  public static final String BUILD_DATE = "2025-03-01 17:36:37 EST";
+  public static final long BUILD_UNIX_TIME = 1740868597819L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,10 +1,18 @@
 package frc.robot;
 
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
+import com.pathplanner.lib.commands.FollowPathCommand;
+import com.pathplanner.lib.commands.PathPlannerAuto;
+import com.pathplanner.lib.path.PathPlannerPath;
+
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -36,7 +44,8 @@ public class RobotContainer {
   public final XboxController operatorController =
       new XboxController(ControllerConstants.OPERATOR_CONTROLLER);
 
-  public LoggedDashboardChooser<Command> autoChooser;
+  public SendableChooser<Command> autoChooser;
+  //public LoggedDashboardChooser<Command> autoChooser;
   public LoggedDashboardChooser<Pose2d> positionChooser;
 
   public final Drivetrain drivetrain =
@@ -81,11 +90,14 @@ public class RobotContainer {
     Trigger operatorLeftTrigger = new Trigger(() -> operatorController.getLeftTriggerAxis() > 0);
     Trigger operatorRightTrigger = new Trigger(() -> operatorController.getRightTriggerAxis() > 0);
 
-    Trigger operatorLeftBumper = new JoystickButton(operatorController, XboxController.Button.kLeftBumper.value);
-    Trigger operatorRightBumper = new JoystickButton(operatorController, XboxController.Button.kRightBumper.value);
+    Trigger operatorLeftBumper =
+        new JoystickButton(operatorController, XboxController.Button.kLeftBumper.value);
+    Trigger operatorRightBumper =
+        new JoystickButton(operatorController, XboxController.Button.kRightBumper.value);
 
-
-    operatorLeftBumper.onTrue(IntakePiece.badIntakePiece(intake, outtake)); //TODO: when we have beambreak on switch to the better command
+    operatorLeftBumper.onTrue(
+        IntakePiece.badIntakePiece(
+            intake, outtake)); // TODO: when we have beambreak on switch to the better command
     operatorRightBumper.whileTrue(new QuickOuttake(outtake));
     // operatorA.whileTrue(new QuickOuttake(outtake));
     operatorDpadDown.onTrue(new ElevatorPositionSet(elevator, ElevatorConstants.INTAKE_POSITION));
@@ -99,19 +111,38 @@ public class RobotContainer {
     operatorY.onTrue(new ElevatorPositionSet(elevator, ElevatorConstants.L4_HEIGHT));
 
     operatorLeftTrigger.whileTrue(new IntakeMove(intake));
-    operatorRightTrigger.whileTrue(new OuttakeSpin(outtake)); //TODO: make these changed based on how much its pressed?
-
-
+    operatorRightTrigger.whileTrue(
+        new OuttakeSpin(outtake)); // TODO: make these changed based on how much its pressed?
   }
 
   private void configureAuto() {
-    autoChooser = new LoggedDashboardChooser<Command>("auto chooser");
+    //autoChooser = new LoggedDashboardChooser<Command>("auto chooser");
     positionChooser = new LoggedDashboardChooser<Pose2d>("position chooser");
+
+    NamedCommands.registerCommand(
+        "elevatorL2", new ElevatorPositionSet(elevator, ElevatorConstants.L2_HEIGHT));
+    NamedCommands.registerCommand(
+        "elevatorL3", new ElevatorPositionSet(elevator, ElevatorConstants.L3_HEIGHT));
+    NamedCommands.registerCommand(
+        "elevatorL4", new ElevatorPositionSet(elevator, ElevatorConstants.L4_HEIGHT));
+    NamedCommands.registerCommand(
+        "elevatorIntakeHeight",
+        new ElevatorPositionSet(elevator, ElevatorConstants.INTAKE_POSITION));
+    NamedCommands.registerCommand("intakePiece", IntakePiece.badIntakePiece(intake, outtake));
+    NamedCommands.registerCommand(
+        "beambreakIntake", IntakePiece.beambreakIntake(intake, outtake, beambreak));
+    NamedCommands.registerCommand("outtakePiece", new QuickOuttake(outtake));
+
+    autoChooser = AutoBuilder.buildAutoChooser();
+    // autoChooser.addOption("StartA_F1_D2", new PathPlannerAuto("A_F1_D2"));
+
+    SmartDashboard.putData("Auto Chooser", autoChooser);
   }
 
   public void startMatch() {}
 
   public Command getAutonomousCommand() {
-    return autoChooser.get();
+    //return autoChooser.get();
+    return autoChooser.getSelected();
   }
 }
