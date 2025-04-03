e40e408b4eebd89ec967aff5160bc94311966055
Ben is dying right now <benjt.mcmillan@icloud.com>
2025-04-03 18:03:30 -0400

    dcmp day 1

--- src/main/java/frc/robot/constants/OuttakeConstants.java
+++ src/main/java/frc/robot/constants/OuttakeConstants.java
@@ -6,18 +6,18 @@ import edu.wpi.first.math.system.LinearSystem;
 import org.ejml.simple.SimpleMatrix;
 
 public class OuttakeConstants {
-  public static final int CURRENT_LIMIT = 30;
+  public static final int CURRENT_LIMIT = 40;
   public static final int MEDIAN_FILTER_SIZE = 12;
 
   public static final double VELOCITY_CONVERSION_FACTOR = 1.0 / 60.0;
   public static final double POSITION_CONVERSION_FACTOR = 1;
 
-  public static final double OUTTAKE_RPS = 30.0;
+  public static final double OUTTAKE_RPS = 20.0;
   public static final double QUICK_OUTTAKE_DURATION = 3; // in seconds
 
   public static final LinearSystem<N1, N1, N1> OUTTAKE_LINEAR_SYSTEM =
-      new LinearSystem<>(
-          new Matrix<>(new SimpleMatrix(new double[] {-5.17})),
+  new Matrix<>(new SimpleMatrix(new double[] {-5.17})),
+  new LinearSystem<>( 
           new Matrix<>(new SimpleMatrix(new double[] {35.62})),
           new Matrix<>(new SimpleMatrix(new double[] {1})),
           new Matrix<>(new SimpleMatrix(new double[] {0.0})));
